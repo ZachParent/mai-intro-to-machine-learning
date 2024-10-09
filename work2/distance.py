12548acab0059873c0911f3625a8f1b074414eb0
Zach Parent <zachparent@duck.com>
2024-10-09 16:40:59 +0200

    add distance.py, knn.py, and svm.py with skeleton function headers

---
+++ work2/distance.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def manhattan_distance(row1: pd.Series, row2: pd.Series) -> float:
+    """
+    Calculates the Manhattan distance between two rows.
+    """
+    # TODO: implement
+    pass
+
+
+def euclidean_distance(row1: pd.Series, row2: pd.Series) -> float:
+    """
+    Calculates the Euclidean distance between two rows.
+    """
+    # TODO: implement
+    pass
+
+
+# TODO: implement another distance metric
