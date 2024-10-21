3e57b7104f6ac5154879cebc59c3955554afb968
Zach Parent <zachparent@duck.com>
2024-10-21 11:39:35 +0200

    ZachParent's changes (#7)
    * Implement load_data in preprocess.ipynb
    
    * add basic preprocessing and a decision tree classifier for hepatitis dataset
    
    * add utils for containing python helper functions
    
    * split preprocess into hepatitis and mushrooms
    
    * remove preprocess
    
    * integrate hepatitis.ipynb and mushroom.ipynb
    
    * fill in the class skeleton
    
    * set classes_ in knn.fit
    
    * add synthetic.ipynb
    
    * implement InverseDistanceWeightedVote
    
    * implement ShepardsWorkVote
    
    * add a test harness to demo hyperparameter product
    
    * use KNN for 1 fold of mushroom data
    
    * add helpers for running KNN and cross validating
    
    * add runtime_estimate.ipynb
    
    * split into model_df and runtime_df
    
    * move Work2_IML_2024_v1.pdf to work2/
    
    * fill preprocess_hepatitis_datasets
    
    * copy run_knn and cross_validate_knn to knn util file
    
    * add a sampling step to prepocess_mushrooms_datasets to speed up mushroom notebook
    
    * add more customizability and documentation for run_knn and cross_validate_knn
    
    * improve analysis of best KNN for hepatitis and mushroom datasets
    
    * add sensitivity, specificity scores to hepatitis
    
    * add header for predict_proba
    
    * add svm and use it for hepatitis
    
    * update hepatitis preprocessing to use knn imputer
    
    * ensure that hepatitis assigns 1 to die class
    
    * add precision-recall curve analysis for svm
    
    * add metrics.py with cross_validate function
    
    * use our cross_validate function when creating the precision recall curve
    
    * remove unneeded svm methods
    
    * move notebooks to match new structure
    
    * Merge branch 'main' into ZachParent

---
+++ work2/tools/ZachParent/__init__.py
@@ -0,0 +1,4 @@
+from .distance import ManhattanDistance, EuclideanDistance
+from .voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
+from .knn import KNNClassifier
+from .metrics import cross_validate
