e34599f9fe3736a27c8eb4d40abfd56f04e95a7e
Zach Parent <zachparent@duck.com>
2024-10-21 11:30:55 +0200

    move notebooks to match new structure

--- work2/tools/__init__.py
+++ work2/tools/__init__.py
@@ -1,4 +1,1 @@
-from .distance import ManhattanDistance, EuclideanDistance
-from .voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
-from .knn import KNNClassifier
-from .metrics import cross_validate
+from tools import *
