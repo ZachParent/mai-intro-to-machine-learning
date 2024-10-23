d94d2f4dad197d24a724eff81c4d4e687dbdc65a
Zach Parent <zachparent@duck.com>
2024-10-23 17:15:46 +0200

    move different models into separate functions

--- work2/tools/run.py
+++ work2/tools/run.py
@@ -1,6 +1,6 @@
 import os
 import itertools
-
+from typing import List, Tuple, Dict
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score
 
@@ -22,42 +22,13 @@ import pathlib
 SCRIPT_DIR = pathlib.Path(__file__).parent.absolute()
 DATA_DIR = os.path.join(SCRIPT_DIR.parent, 'data')
 
-
-def run():
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Run the model with a specific dataset.')
-    parser.add_argument('--dataset_name', type=str, choices=['hepatitis', 'mushroom'],
-                        help='The name of the dataset to use (hepatitis or mushroom)')
-    parser.add_argument('--verbose', '-v', action='store_true',
-                        help='Whether to print verbose output')
-    args = parser.parse_args()
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-
-    # Configurations
-    dataset_name = args.dataset_name
-    processing_funcs_per_ds = {"hepatitis": preprocess_hepatitis_datasets, "mushroom": preprocess_mushroom_datasets}
-    class_columns_per_ds = {"hepatitis": "Class", "mushroom": "class"}
-
-    logging.debug(f"Running {dataset_name}...")
-    train_path = f'{DATA_DIR}/raw/{dataset_name}/*train.arff'
-    test_path = f'{DATA_DIR}/raw/{dataset_name}/*test.arff'
-    train_dfs = load_datasets(train_path)
-    test_dfs = load_datasets(test_path)
-
-    logging.debug(f"train_path: {train_path}")
-    train_dfs = [processing_funcs_per_ds[dataset_name](df) for df in train_dfs]
-    test_dfs = [processing_funcs_per_ds[dataset_name](df) for df in test_dfs]
-    logging.debug(f"Train datasets count: {len(train_dfs)}")
-    logging.debug(f"Test datasets count: {len(test_dfs)}")
-
-    # Fit weighting function
-    full_data = pd.concat([train_dfs[0], test_dfs[0]])
-    full_data_X = full_data.drop(columns=[class_columns_per_ds[dataset_name]])
-    full_data_y = full_data[class_columns_per_ds[dataset_name]]
-
-    # ========== SVM ==========
-
+def run_svm(train_dfs: List[pd.DataFrame], 
+            test_dfs: List[pd.DataFrame], 
+            dataset_name: str, 
+            class_columns_per_ds: Dict[str, str]) -> None:
+    """
+    Run SVM classification with various parameter configurations.
+    """
     # SVM Parameters
     c_values = [1, 3, 5, 7]
     kernel_types = ["linear", "poly", "rbf", "sigmoid"]
@@ -84,7 +55,7 @@ def run():
             kernel=kernel_type,
         )
 
-        logging.info(f"Running SVM: [kernel={kernel_type}, C={C}]")
+        logging.debug(f"Running SVM: [kernel={kernel_type}, C={C}]")
 
         y_trues_all, y_preds_all = [], []
         total_train_time, total_test_time = 0.0, 0.0
@@ -129,8 +100,15 @@ def run():
     file_path_svm = os.path.join(DATA_DIR, "results", f'svm_{dataset_name}.csv')
     results_svm.to_csv(file_path_svm, index=False)
 
-    # ========== KNN ==========
-
+def run_knn(train_dfs: List[pd.DataFrame], 
+            test_dfs: List[pd.DataFrame], 
+            dataset_name: str, 
+            class_columns_per_ds: Dict[str, str], 
+            full_data_X: pd.DataFrame, 
+            full_data_y: pd.Series) -> Tuple[KNNClassifier, Dict[str, np.ndarray]]:
+    """
+    Run KNN classification with various parameter configurations.
+    """
     # Define CM for MahalanobisDistance
     covariance_matrix = np.cov(train_dfs[0].drop(columns=["Class"]).apply(pd.to_numeric, errors='coerce'),
                                 rowvar=False)
@@ -159,6 +137,7 @@ def run():
         ]
     )
 
+    logging.info("Fitting weighting functions")
     weights = {}
     for weighting_func in weighting_funcs:
         weighting_func.fit(full_data_X, full_data_y)
@@ -177,7 +156,7 @@ def run():
             weights=weights[weighting_func.__class__.__name__],
         )
 
-        logging.info(
+        logging.debug(
             f"Running KNN: [weighting_func={weighting_func.__class__.__name__}, distance_func={distance_func.__class__.__name__}, voting_func={voting_func.__class__.__name__}, k={k}]")
 
         y_trues_all, y_preds_all = [], []
@@ -219,16 +198,21 @@ def run():
     file_path = os.path.join(DATA_DIR, "results", f'knn_{dataset_name}.csv')
     results.to_csv(file_path, index=False)
 
-    # Extract the best configuration instance
+    return best_config_instance, weights
+
+def run_reduced_knn(train_dfs: List[pd.DataFrame], 
+                    test_dfs: List[pd.DataFrame], 
+                    dataset_name: str, 
+                    class_columns_per_ds: Dict[str, str], 
+                    best_config_instance: Dict[str, any], 
+                    weights: Dict[str, np.ndarray]) -> None:
+    """
+    Run reduced KNN classification with various techniques.
+    """
     best_k = best_config_instance["k"]
     best_distance_func = best_config_instance["distance_func"]
     best_voting_func = best_config_instance["voting_func"]
     best_weighting_func = best_config_instance["weighting_func"]
-
-    logging.info(
-        f"Best configuration: k={best_k}, distance_func={best_distance_func.__class__.__name__}, voting_func={best_voting_func.__class__.__name__}, weighting_func={best_weighting_func.__class__.__name__}")
-
-    # ========== Reduced KNN ==========
 
     reduction_funcs = {"control": lambda x, y, z: (x, y), "cnn": condensed_nearest_neighbor, "enn": edited_nearest_neighbor, "drop2": drop2}
     reduction_results = pd.DataFrame(
@@ -259,7 +243,7 @@ def run():
         logging.info(f"Running KNN with reduction: {reduction_func}")
 
         y_trues_all, y_preds_all = [], []
-        total_train_time, total_test_time = 0.0, 0.0
+        total_train_time, total_test_time, total_storage = 0.0, 0.0, 0
 
         for train_df, test_df in zip(train_dfs, test_dfs):
             X_train = train_df.drop(columns=[class_columns_per_ds[dataset_name]])
@@ -267,9 +251,8 @@ def run():
             X_test = test_df.drop(columns=[class_columns_per_ds[dataset_name]])
             y_test = test_df[class_columns_per_ds[dataset_name]]
 
-            logging.info(f"Reducing training data with {reduction_func}")
+            logging.debug(f"Reducing training data with {reduction_func}")
             X_train_reduced, y_train_reduced = reduction_funcs[reduction_func](X_train, y_train, best_k)
-            logging.info(f"Reduced training data storage: {len(X_train_reduced)}")
 
             storage = len(X_train_reduced)
 
@@ -280,15 +263,16 @@ def run():
             # Update totals
             total_train_time += train_time
             total_test_time += test_time
-
+            total_storage += storage
             # Collect true labels and predictions
             y_trues_all.extend(y_true)
             y_preds_all.extend(y_pred)
+            logging.debug(f"Reduced training data storage: {len(X_train_reduced)} / {len(X_train)}. Took {train_time} seconds to train and {test_time} seconds to test.")
 
         # Compute confusion matrix and accuracy
         cm = confusion_matrix(y_trues_all, y_preds_all)
         tn, fp, fn, tp = cm.ravel()
-        accuracy = accuracy_score(y_trues_all, y_preds_all)
+        
 
         # Append the results
         reduction_results.loc[len(reduction_results)] = [
@@ -303,12 +287,59 @@ def run():
             fn,
             total_train_time,
             total_test_time,
-            storage
+            total_storage
         ]
 
-        # Save the results for reduced KNN
-        file_path_reduction = os.path.join(DATA_DIR, "results", f'knn_reduction_{dataset_name}.csv')
-        reduction_results.to_csv(file_path_reduction, index=False)
+    # Save the results for reduced KNN
+    file_path_reduction = os.path.join(DATA_DIR, "results", f'knn_reduction_{dataset_name}.csv')
+    reduction_results.to_csv(file_path_reduction, index=False)
+
+
+def run():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Run the model with a specific dataset.')
+    parser.add_argument('--dataset_name', type=str, choices=['hepatitis', 'mushroom'],
+                        help='The name of the dataset to use (hepatitis or mushroom)')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='Whether to print verbose output')
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    # Configurations
+    dataset_name = args.dataset_name
+    processing_funcs_per_ds = {"hepatitis": preprocess_hepatitis_datasets, "mushroom": preprocess_mushroom_datasets}
+    class_columns_per_ds = {"hepatitis": "Class", "mushroom": "class"}
+
+    logging.debug(f"Using {dataset_name} dataset...")
+    train_path = f'{DATA_DIR}/raw/{dataset_name}/*train.arff'
+    test_path = f'{DATA_DIR}/raw/{dataset_name}/*test.arff'
+    train_dfs = load_datasets(train_path)
+    test_dfs = load_datasets(test_path)
+
+    logging.debug(f"train_path: {train_path}")
+    train_dfs = [processing_funcs_per_ds[dataset_name](df) for df in train_dfs]
+    test_dfs = [processing_funcs_per_ds[dataset_name](df) for df in test_dfs]
+    logging.debug(f"Train datasets count: {len(train_dfs)}")
+    logging.debug(f"Test datasets count: {len(test_dfs)}")
+
+    full_data = pd.concat([train_dfs[0], test_dfs[0]])
+    full_data_X = full_data.drop(columns=[class_columns_per_ds[dataset_name]])
+    full_data_y = full_data[class_columns_per_ds[dataset_name]]
+
+    # ========== SVM ==========
+
+    run_svm(train_dfs, test_dfs, dataset_name, class_columns_per_ds)
+
+    # ========== KNN ==========
+
+    best_config_instance, weights = run_knn(train_dfs, test_dfs, dataset_name, class_columns_per_ds, full_data_X, full_data_y)
+
+    # ========== Reduced KNN ==========
+
+    run_reduced_knn(train_dfs, test_dfs, dataset_name, class_columns_per_ds, best_config_instance, weights)
 
 if __name__ == '__main__':
     run()
