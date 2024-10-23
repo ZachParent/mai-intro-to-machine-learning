e0098b8b7f13ad6c2d1ef19f2fe78fa7cb11cff0
Zach Parent <zachparent@duck.com>
2024-10-23 14:54:39 +0200

    move __init__.py back into tools/

--- work2/__init__.py
+++
@@ -1,4 +0,0 @@
-from work2.tools.distance import EuclideanDistance, ManhattanDistance, ChebyshevDistance
-from work2.tools.voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
-from work2.tools.weighting import EqualWeighting, InformationGainWeighting, ReliefFWeighting
-from work2.tools.knn import KNNClassifier
---
+++ work2/tools/__init__.py
@@ -0,0 +1,4 @@
+from tools.distance import EuclideanDistance, ManhattanDistance, ChebyshevDistance
+from tools.voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
+from tools.weighting import EqualWeighting, InformationGainWeighting, ReliefFWeighting
+from tools.knn import KNNClassifier
