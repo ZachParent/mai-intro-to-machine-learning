0e367499ce701fa1181a4c26a192577ccb8ca3e3
Zach Parent <zachparent@duck.com>
2024-11-03 01:03:32 +0100

    remove tools_old and .python-version and data/preprocessed

--- work2/tools_old/CarlosJimenez/__init__.py
+++
@@ -1,4 +0,0 @@
-from .distance import ManhattanDistance, EuclideanDistance
-from .voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
-from .knn import KNNClassifier
-from .metrics import cross_validate
--- work2/tools_old/KacperWork/__init__.py
+++
@@ -1,3 +0,0 @@
-from .distance import ManhattanDistance, EuclideanDistance
-from .voting import MajorityClassVote, InverseDistanceWeightedVote, ShepardsWorkVote
-from .knn import KNNClassifier
