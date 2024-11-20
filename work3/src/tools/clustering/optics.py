3c52a82b5f73769c67334a3b1f151fb9278ab435
Zach Parent <zachparent@duck.com>
2024-11-20 11:21:01 +0100

    choose more appropriate params for optics and spectral_clustering

--- work3/src/tools/clustering/optics.py
+++ work3/src/tools/clustering/optics.py
@@ -1,7 +1,10 @@
+import numpy as np
 from sklearn.cluster import OPTICS
 
 OpticsParamsGrid = {
-    "min_samples": [3, 5, 7],
+    "min_samples": [3, 5, 10],
+    "max_eps": [np.inf, 2.0],
+    "cluster_method": ["dbscan", "xi"],
 }
 
 
--- work3/src/tools/clustering/spectral_clustering.py
+++ work3/src/tools/clustering/spectral_clustering.py
@@ -1,7 +1,8 @@
 from sklearn.cluster import SpectralClustering
 
 SpectralClusteringParamsGrid = {
-    "degree": [1, 2, 3],
+    "n_clusters": [2, 4, 8],
+    "gamma": [0.5, 1, 2],
 }
 
 
