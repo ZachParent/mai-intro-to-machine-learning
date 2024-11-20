74af3ddf08044a7a431f88bb2866a60b4df0e976
SheenaGit <91433803+SheenaGit@users.noreply.github.com>
2024-11-20 10:01:05 +0100

    Added initial k-means algorithm

--- work3/src/tools/clustering/kmeans.py
+++ work3/src/tools/clustering/kmeans.py
@@ -1,9 +1,47 @@
-from sklearn.cluster import KMeans
+import random
+import numpy as np
+import pandas as pd
+from tools.distance import ManhattanDistance, EuclideanDistance, ChebyshevDistance
 
 KMeansParamsGrid = {
-    "n_clusters": [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "k": [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "distance-metrics": [ManhattanDistance(), EuclideanDistance(), ChebyshevDistance()]
 }
 
+class KMeans:
+    def __init__(self, **kwargs):
+        self.k = kwargs.get("k", 3)
+        self.distance_metric = kwargs.get("distance_metric", EuclideanDistance())
+        self.max_iterations = kwargs.get("max_iterations", 300)
+        self.tolerance = kwargs.get("tolerance", 1e-4)
+        self.centroids = None
 
-class KMeans(KMeans):
-    pass
+    def fit(self, data):
+        data = data.to_numpy() if isinstance(data, pd.DataFrame) else data
+        n_samples, n_features = data.shape
+        self.centroids = data[random.sample(range(n_samples), self.k)]
+
+        for _ in range(self.max_iterations):
+            # Assign clusters
+            clusters = self._assign_clusters(data)
+
+            # Update centroids
+            new_centroids = np.array([data[clusters == i].mean(axis=0) for i in range(self.k)])
+
+            # Check for convergence
+            if np.all(np.abs(new_centroids - self.centroids) < self.tolerance):
+                break
+
+            self.centroids = new_centroids
+
+        return self.centroids, clusters
+
+    def fit_predict(self, data):
+        _, clusters = self.fit(data)
+        return clusters
+
+    def _assign_clusters(self, data):
+        distances = np.array(
+            [[self.distance_metric.calculate(point, centroid) for centroid in self.centroids] for point in data]
+        )
+        return np.argmin(distances, axis=1)
