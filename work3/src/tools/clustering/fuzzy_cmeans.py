98f4462983a5d2fc6a4bff3b9ebf2ae48752130e
Zach Parent <zachparent@duck.com>
2024-11-20 10:09:12 +0100

    simplify fit method

--- work3/src/tools/clustering/fuzzy_cmeans.py
+++ work3/src/tools/clustering/fuzzy_cmeans.py
@@ -3,18 +3,16 @@ from sklearn.base import BaseEstimator, ClusterMixin
 
 FuzzyCMeansParamsGrid = {
     "n_clusters": [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "fuzzyness": [1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5],
 }
 
 
 class FuzzyCMeans(ClusterMixin, BaseEstimator):
-    def __init__(self, n_clusters: int):
+    def __init__(self, n_clusters: int, fuzzyness: float):
         self.n_clusters = n_clusters
-        self.cluster_centers_ = None
+        self.fuzzyness = fuzzyness
 
-    def fit(self, X, y=None):
+    def fit(self, X):
         self.labels_ = np.ones(shape=(len(X),), dtype=np.int64)
         self.is_fitted_ = True
         return self
-
-    def predict(self, X):
-        return np.ones(shape=(len(X),), dtype=np.int64)
