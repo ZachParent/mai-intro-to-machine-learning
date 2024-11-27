83767d65c3347043bfba629657a1f9c2d847933b
Zach Parent <zachparent@duck.com>
2024-11-27 15:49:39 +0100

    expand params for optics and spectral_clustering

--- work3/src/tools/clustering/optics.py
+++ work3/src/tools/clustering/optics.py
@@ -2,9 +2,11 @@ import numpy as np
 from sklearn.cluster import OPTICS
 
 OpticsParamsGrid = {
-    "metric": ["euclidean", "l1", "manhattan"],
-    # TODO(Zach): choose just 2
-    "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
+    "metric": ["euclidean", "manhattan"],
+    "algorithm": ["auto", "ball_tree"],
+    "min_samples": [5, 10, 20],
+    "xi": [0.01, 0.05, 0.1],
+    "min_cluster_size": [5, 10, 20]
 }
 
 
--- work3/src/tools/clustering/spectral_clustering.py
+++ work3/src/tools/clustering/spectral_clustering.py
@@ -1,10 +1,12 @@
 from sklearn.cluster import SpectralClustering
+from tools.config import RANDOM_STATE
 
 SpectralClusteringParamsGrid = {
     "n_neighbors": [5, 10, 20],
     "affinity": ["nearest_neighbors", "rbf"],
     "eigen_solver": ["arpack", "lobpcg"],
     "assign_labels": ["kmeans", "cluster_qr"],
+    "random_state": RANDOM_STATE,
 }
 
 
