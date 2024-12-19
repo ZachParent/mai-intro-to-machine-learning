4e3334b31ee74fcbac286c4b21199a85e0f5fd58
Zach Parent <zachparent@duck.com>
2024-12-19 10:25:33 -0500

    update kernel pca params to use more n_components

--- work4/src/tools/dimensionality_reduction/kernel_pca.py
+++ work4/src/tools/dimensionality_reduction/kernel_pca.py
@@ -1,5 +1,5 @@
 KERNEL_PCA_PARAMS_GRID_MAP = {
-    "n_components": [2, 3],
+    "n_components": [2, 3, 4, 10, 11, 12],
     "kernel": ["poly", "rbf"],  # RBF for non-linear data, poly for structured data
     "gamma": [
         0.1,
