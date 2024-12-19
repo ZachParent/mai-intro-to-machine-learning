a37bc0acdf4fbec23742b39ea0d57592444597b4
Zach Parent <zachparent@duck.com>
2024-12-19 07:50:04 -0500

    update dimensionality_reduction.__init__

--- work4/src/tools/dimensionality_reduction/kernel_pca.py
+++ work4/src/tools/dimensionality_reduction/kernel_pca.py
@@ -1,5 +1,3 @@
-from sklearn.decomposition import KernelPCA as SklearnKernelPCA
-
 KERNEL_PCA_PARAMS_GRID_MAP = {
     "n_components": [2, 3],
     "kernel": ["poly", "rbf"],  # RBF for non-linear data, poly for structured data
@@ -8,7 +6,3 @@ KERNEL_PCA_PARAMS_GRID_MAP = {
         1,
     ],  # For rbf and poly kernels - defines how far the influence of a single training sample reaches
 }
-
-
-class KernelPCA(SklearnKernelPCA):
-    pass
